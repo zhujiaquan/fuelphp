3a03f907f1e1fa7d25f8ec1289722ac04401179e
jquanzhu <jquan.zhu@gmail.com>
2013-05-03 17:43:40 +0800

    ci

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package     Fuel
+ * @version     1.5
+ * @author      Fuel Development Team
+ * @license     MIT License
+ * @copyright   2010 - 2013 Fuel Development Team
+ * @link        http://fuelphp.com
+ */
+
+return array (
+	'crypto_key' => '6-oUOYIggqLAEsEnJscbob7o',
+	'crypto_iv' => 'qysqd4EusGMMGSIYxIzK8W0s',
+	'crypto_hmac' => 'fVEo9Ew_w--gGCcRMg5Ecp-g',
+);
